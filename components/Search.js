52261dc06e43be0f11eb6d8c5e3a85d439898eeb
Olga Furmanowska <olgafurmanowska@gmail.com>
2018-05-28 08:29:23 +0200

    Update Search.js file's content

--- components/Search.js
+++ components/Search.js
@@ -10,17 +10,28 @@ Search = React.createClass({
       this.setState({
         searchingText: searchingText
       });
-    };
+
+      if (searchingText.length >2) {
+        this.props.onSearch(searchingText);
+      }
+    },
+
+    handleKeyUp: function(event) {
+      if (event.keyCode === 13) {
+        this.props.onSearch(this.state.searchingText);
+      }
+    },
 
     render: function() {
       var styles = {
           fontSize: '1.5em',
           width: '90%',
           maxWidth: '350px'
-      },
+      };
       return <input
                 type="text"
                 onChange={this.handleChange}
+                onKeyUp={this.handleKeyUp}
                 placeholder="Enter a search term here"
                 style={styles}
                 value={this.state.searchTerm}
